9206106f1466cddb3caf7c32e786fe05ddeb08cd
Teme <lememta@gmail.com>
2016-09-14 17:33:12 -0700

    add type and bb checking

--- crab_lang/crabGrammar.py
+++ crab_lang/crabGrammar.py
@@ -2,7 +2,10 @@ from pyparsing import *
 from LogManager import LoggingManager
 import pprint
 
+debug = False
 
+class CrabParserException(Exception):
+    pass
 
 class CrabParser(object):
 
@@ -12,45 +15,82 @@ class CrabParser(object):
                      "basic_blocks" : {},
                      "edges": {},
         }
-    
+        self.Vars = list()
+        self.BB = list()
         return
 
 
     def _decl(self, decl):
         for d in decl:
             var = d[0]
+            self.Vars.append(var)
             typ = d[1]
             decl =self.CRAB_PROG["decl"]
             decl.update({d[0]:d[1]})
         return
 
+    def _checkVars(self, action, expr_dict):
+        vars = list()
+        for exp_type, exp in expr_dict.iteritems():
+            if exp_type == "mexp":
+                for e in exp:
+                    vars.append(e[1])                
+            else:
+                vars.append(exp[1])
+        if not set(vars).issubset(set(self.Vars)):
+            v = " ,".join(x for x in set(vars) - set(self.Vars))
+            msg = "In %s: %s is not declared" % (action,v)
+            raise CrabParserException(msg)
+        else:
+            if debug: print "OK vars"
+
+    def _mkLinearCst(self, cst):
+        cst_out = dict()
+        for c, e in cst.iteritems():
+            if c == "bop":
+                if type(e) is list:
+                    cst_out.update({'bop':e[0]})
+                else:
+                    cst_out.update({'bop':e})
+            elif c == "l_exp":
+                cst_out.update({'l_exp':e.values()[0]})
+            elif c == "num":
+                cst_out.update({c:e})
+        return cst_out
+                
+        
     def _basicBlocks(self, bbs):
         bb_dict = dict()
         for bb in bbs:
             bb_name = bb.bb_name[0]
-            bb_instr = bb.instr
+            self.BB.append(bb_name)
             bb_dict.update({bb_name:{}})
             inst_n = 0
             instr_dict = dict()
-            for inst in bb_instr:
-                if inst.getName() is "assignment":
-                    instr_dict.update({inst_n:{"assignment":inst.asList()}})
-                elif inst.getName() is "assume":
-                    assume_dict = inst.asDict()['cst']
-                    if type(assume_dict['bop']) is list:
-                        assume_dict.update({'bop':(assume_dict['bop'])[0]})
-                    instr_dict.update({inst_n:{"assume":assume_dict}})
-                elif inst.getName() is "assert":
-                    ass_dict = inst.asDict()['cst']
-                    if type(ass_dict['bop']) is list:
-                        ass_dict.update({'bop':(ass_dict['bop'])[0]})
-                    instr_dict.update({inst_n:{"assert":ass_dict}})
-                elif inst.getName() in ["add", "sub", "mul", "div"]:
-                    instr_dict.update({inst_n:{str(inst.getName()):inst.asList()}})
-                elif inst.getName() is "havoc":
-                    instr_dict.update({inst_n:{"havoc":inst.asList()}})
+            tbd_dict = bb.instr.asDict()
+            for inst_name, inst in tbd_dict.iteritems():
+                if inst_name == "assignment":
+                    assignment = [inst['rhs'][0], inst['lhs'][0]]
+                    instr_dict.update({inst_n:{"assignment":assignment}})
+                elif inst_name == "assume" or inst_name == "assertion":
+                    cst = inst['cst']
+                    self._checkVars(inst_name, cst['l_exp'])
+                    linear_cst = self._mkLinearCst(cst)
+                    instr_dict.update({inst_n:{inst_name:linear_cst}})
+                elif inst_name in ["add", "sub", "mul", "div"]:
+                    if not set(inst).issubset(set(self.Vars)):
+                        v = " ,".join(x for x in set(inst) - set(self.Vars))
+                        msg = "In %s: %s is not declared" % (inst_name, v)
+                        raise CrabParserException(msg)
+                    instr_dict.update({inst_n:{inst_name:inst}})
+                elif inst_name is "havoc":
+                    if inst[0] not in self.Vars:
+                        msg = "In Havoc %s is not declared" % h_var[0]
+                        raise CrabParserException(msg)
+                    instr_dict.update({inst_n:{"havoc":inst}})
                 else:
-                    assert False, 'unknown instruction'
+                    msg = "%s unknown instruction" % inst_name
+                    raise CrabParserException(msg)
                 inst_n +=1
             bb_dict.update({bb_name:instr_dict})
         (self.CRAB_PROG["basic_blocks"]).update(bb_dict)
@@ -62,6 +102,10 @@ class CrabParser(object):
         n = 0
         for edge in edges:
             edge = edge.asList()
+            if not set(edge).issubset(set(self.BB)):
+                v = " ,".join(x for x in set(edge) - set(self.BB))
+                msg = "\t %s not present"% v
+                raise CrabParserException(msg)
             out.update({n:edge})
             n+=1
         (self.CRAB_PROG["edges"]).update(out)
@@ -70,23 +114,28 @@ class CrabParser(object):
             
         
     def _crab_prog(self, s,l,c):
+        error = False
         for g in c.abs_domain:
             self.CRAB_PROG.update({"abs_domain": g})
         try:
             self._decl(c.decl)
         except Exception as e:
-            print "Getting variable declartion"
+            print "Error: Handling variable declartions\n\t %s" % e
+            error = True
         try:
             self._basicBlocks(c.bbs)
         except Exception as e:
-            print e
-            print "Handling basic blocks"
+            print "Error: Handling basic blocks\n\t %s" % e
+            error = True
         try:
             self._edges(c.edges)
         except Exception as e:
-            print e
-            print "Handling edges"
-        return self.CRAB_PROG
+            print "Error: Handling Links\n\t %s" % e
+            error = True
+        if not error:
+            return self.CRAB_PROG
+        else:
+            return {}
 
     def _globals(self, s, l, c):
         print c
@@ -123,15 +172,15 @@ class CrabParser(object):
 
     EXP = Group(Literal("(").suppress()\
                 + NUM + Literal("*").suppress()\
-                + VARS + Literal(")").suppress()).setResultsName("")
-    LINEAR_EXP = OneOrMore(
-        Group(EXP + Literal("+").suppress() + EXP)
-        | EXP).setResultsName("exp")
+                + VARS + Literal(")").suppress())
+    
+    LINEAR_EXP = OneOrMore(Group(EXP + Literal("+").suppress() + EXP).setResultsName("mexp") | EXP.setResultsName("sexp"))
+    
     BOP = Literal("<") | Literal(">") | Literal("=")\
           | Group(Literal("!=")) | Group(Literal(">="))\
           | Group(Literal(">="))
 
-    LINEAR_CST = Group(LINEAR_EXP.setResultsName("exp") + BOP.setResultsName("bop") + NUM.setResultsName("num"))
+    LINEAR_CST = Group(LINEAR_EXP.setResultsName("l_exp") + BOP.setResultsName("bop") + NUM.setResultsName("num"))
 
     ASSIGNMENT = Group(VARS.setResultsName("lhs") + Literal(":=").suppress()\
                        + VARS.setResultsName("rhs") + Literal(";").suppress())
@@ -150,7 +199,7 @@ class CrabParser(object):
 
     INSTR = ASSIGNMENT.setResultsName("assignment")\
             | ASSUME.setResultsName("assume")\
-            | ASSERT.setResultsName("assert")\
+            | ASSERT.setResultsName("assertion")\
             | ADD.setResultsName("add")\
             | SUB.setResultsName("sub")\
             | MUL.setResultsName("mul")\
@@ -254,15 +303,15 @@ edges: {
 
 test = """
 abs_domain : interval;
-decl : [h:int; k:int;];
+decl : [h:int; k:int; v1:int; v2:int; v3:int; v4:int;];
 bb {
-   bb1[h:=k; u:=k; h:=v; assert (-3*h)+(4*h)!=0;]
+   bb1[h:=k; u:=k; h:=v; assert (-3*h)+(4*v2)!=0;]
    bb2[havoc h;]
    bb3[]
-   bb4[assume (2*h)=0;]
-   bb5 [v1 = v2 + v3; v1 = v2 - v4;]
+   bb4[assume (2*h)>0;]
+   bb5 [v1 = v2 + v3; v1 = v2 - v3; havoc v3;]
    }
-link {bb1 >> bb2; bb2 >> bb3; bb4>>bb5;}
+link {bb1 >> bb2; bb2 >> bb3; bb4>>bb5; bb6 >> bb7;}
 """
 
 if __name__ == "__main__":
