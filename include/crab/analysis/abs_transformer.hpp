071996e5c3897e968d1e1fc3ba3cc0ca4b6361c2
Yugesh Kothari <yugesh.kothari@gmail.com>
2020-07-19 22:05:23 +0530

    Added is_valid_velocity intrinsic

--- include/crab/analysis/abs_transformer.hpp
+++ include/crab/analysis/abs_transformer.hpp
@@ -45,6 +45,7 @@
 //Includes for DeepSymbol
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 #include <iterator>
 #include <algorithm>
 #include <map>
@@ -715,6 +716,83 @@ public:
     }
 
     return 50;
+  }
+
+  int is_valid_position_nointrinsic(int px, int py)
+  {
+    int map_pos = 0;
+    if((px >= 0)&&(px <14) && (py>=0) && (py <14))
+    {
+	   map_pos = 0;
+    }
+    else if(px == 24 || py == 24)
+    {
+	   map_pos = 0;
+    }
+    else if(px < 0)
+    {
+	   map_pos = 0;
+    }
+    else if (px > 24)
+    {
+	   map_pos = 0;
+    }
+    else if(py < 0)
+    {
+	   map_pos = 0;
+    }
+    else if (py > 24)
+    {
+	   map_pos = 0;
+    }
+    else 
+    {
+	   map_pos = 1;
+    }
+
+    return map_pos;
+  }
+
+  int access_velocity_traversed_position(int a, int b, int c, int d){
+    int velocity_to_traversed_positions[6][6][6][6] =
+    {{{{1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 1, 1, 1, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}}},
+     {{{1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 1}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}}},
+     {{{1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 0}, {0, 0, 0, 0, 1, 1}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}}},
+     {{{1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 0}, {0, 0, 0, 0, 1, 1}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}}},
+     {{{1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 0}, {0, 0, 0, 0, 1, 1}, {0, 0, 0, 0, 0, 0}}},
+     {{{1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 1, 0, 0}},
+      {{1, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 0}, {0, 0, 0, 0, 1, 0}},
+      {{1, 1, 0, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 0}, {0, 0, 0, 0, 1, 1}, {0, 0, 0, 0, 0, 1}}}};
+
+      return velocity_to_traversed_positions[a][b][c][d];
   }
   
   void exec(intrinsic_t &cs) {
@@ -1844,7 +1922,7 @@ public:
             if(input_bounds[i].second == -100){
               input_bounds[i].second = 5;
             }
-          }
+          }   
         }
 
         //Use Input_bounds to compute wall distance
@@ -2106,6 +2184,241 @@ public:
 
       crab::outs() << "Projected invariants at exit : " << tmp1 << "\n**********\n";
 
+    }
+    else if(cs.get_intrinsic_name() == "is_valid_velocity"){
+      AbsD pre_inv(m_inv);
+
+      std::vector<var_t> args_list = cs.get_args();
+      var_t var_isvalid = args_list[4];
+      //Forget what we know about distance
+      m_inv -= var_isvalid;
+
+      //get string name of input variables
+      std::string call_st = cs.get_string();
+      crab::outs() << "This is the call statement : " << call_st << "\n";
+      call_st = call_st.substr(call_st.find("(") + 1, call_st.find(")") - call_st.find("(") - 1);
+      std::istringstream iss1(call_st);
+      int index = 0;
+      std::vector<std::string> llvm_Vars;
+      std::string item;
+      while(std::getline(iss1, item, ',')){
+        if(index == 0){
+          if(item.substr(0, item.find(":")) != "is_valid_velocity"){
+            crab::outs() << "Malformed instrinsic statement call" << "\n";
+            std::exit(1);
+          }
+        }
+        else if(index >=1 && index <=5){
+          llvm_Vars.push_back(item.substr(0, item.find(":")));
+        }
+        else{
+          crab::outs() << "More than expected arguements passed" << "\n";
+          std::exit(1);
+        }
+        index++;
+      }
+
+      auto pre_inv_boxes = m_inv.get_content_domain();
+      pre_inv_boxes.project(cs.get_args());
+      auto djct_csts = pre_inv_boxes.to_disjunctive_linear_constraint_system();
+      crab::outs() << "Projected invariants at entry : " << djct_csts << "\n";
+
+      //Iterate over each disjunct in djct_csts
+      abs_dom_t new_m_inv = abs_dom_t::bottom();
+      for(auto &d_ct: djct_csts){
+        std::string invars = d_ct.get_string(); //get string of preconds invariants
+        crab::outs() << "\n\nThis is the linear cst in a disjunct : " << invars << "\n";
+        if(invars.size() < 2){
+          crab::outs() << "Malformed lin_cst string in intrinsic (check variable pre_invars)" << "\n";
+          std::exit(1);
+        }
+
+        invars = invars.substr(1, invars.size()-2); //Stripped braces
+
+        std::vector<std::string> lin_cst;
+        std::vector<std::vector<std::string>> tokens;
+        std::istringstream iss2(invars);
+        while(std::getline(iss2, item, ';')){
+          item = trim(item);
+          std::stringstream ss(item);  //String of individual lin_cst
+          std::istream_iterator<std::string> begin(ss);
+          std::istream_iterator<std::string> end;
+          std::vector<std::string> lin_cst(begin, end); //Convert each linear_cst to its tokens
+          tokens.push_back(lin_cst);
+        }
+
+        std::vector<std::pair<int, int>> input_bounds(4, std::make_pair(-100,-100));
+        for(auto it: tokens){
+          if((it.size()!=3) && (it[0]!= "true") && (it[0]!= "false")){
+            crab::outs() << "Malformed lin_cst token. Exitting" << "\n";
+            std::exit(1);
+          }
+          else if(it.size()==3){
+            item = it[0]; //String of llvm variable
+            if(item.at(0)=='-'){
+              item = item.substr(1, item.size()-1);
+              auto index_it = std::find(llvm_Vars.begin(), llvm_Vars.end(), item);
+              if(index_it == llvm_Vars.end()){
+                crab::outs() << "Variable not found. Exitting\n";
+                std::exit(1);
+              }
+
+              int index = std::distance(llvm_Vars.begin(), index_it);
+              if(it[1] == "="){
+                input_bounds[index].first = -1*std::stoi(it[2]);
+                input_bounds[index].second = -1*std::stoi(it[2]);
+              }
+              else if(it[1] == "<"){
+                input_bounds[index].first = -1*std::stoi(it[2])+1;
+              }
+              else if(it[1] == "<="){
+                input_bounds[index].first = -1*std::stoi(it[2]);
+              }
+              else if(it[1] == ">"){
+                input_bounds[index].second = -1*std::stoi(it[2])-1;
+              }
+              else if(it[1] == ">="){
+               input_bounds[index].second = -1*std::stoi(it[2]);
+              }
+              else{
+                crab::outs() << "LIN CST OPERATOR INVALID. EXITTING" << "\n";
+                exit(1);
+              }
+            }
+            else{
+              auto index_it = std::find(llvm_Vars.begin(), llvm_Vars.end(), item);
+              if(index_it == llvm_Vars.end()){
+                crab::outs() << "Variable not found. Exitting";
+                std::exit(1);
+              }
+
+              int index = std::distance(llvm_Vars.begin(), index_it);
+              if(it[1] == "="){
+                input_bounds[index].first = std::stoi(it[2]);
+                input_bounds[index].second = std::stoi(it[2]);
+              }
+              else if(it[1] == "<"){
+                input_bounds[index].second = std::stoi(it[2])-1;
+              }
+              else if(it[1] == "<="){
+                input_bounds[index].second = std::stoi(it[2]);
+              }
+              else if(it[1] == ">"){
+                input_bounds[index].first = std::stoi(it[2])+1;
+              }
+              else if(it[1] == ">="){
+               input_bounds[index].first = std::stoi(it[2]);
+              }
+              else{
+                crab::outs() << "LIN CST OPERATOR INVALID. EXITTING" << "\n";
+                exit(1);
+              }
+            }
+          }
+        }
+
+        //Sanitize input_bounds if uninitialized
+        for(int i=0;i<4;i++){
+          if(i==0||i==1){
+            if(input_bounds[i].first == -100){
+              input_bounds[i].first = 0;
+            }
+            if(input_bounds[i].second == -100){
+              input_bounds[i].second = 25;
+            }
+          }
+          else{
+            if(input_bounds[i].first == -100){
+              input_bounds[i].first = 0;
+            }
+            if(input_bounds[i].second == -100){
+              input_bounds[i].second = 5;
+            }
+          }   
+        }
+
+        std::unordered_set<int> validity; 
+
+        //Use Input_bounds to check if velocity is valid
+        for(int px = input_bounds[0].first; px <= input_bounds[0].second; px++){
+          for(int py = input_bounds[1].first; py <= input_bounds[1].second; py++){
+            for(int vx = input_bounds[2].first; vx <= input_bounds[2].second; vx++){
+              for(int vy = input_bounds[3].first; vy <= input_bounds[3].second; vy++){
+                int sign_vx = vx >= 0 ? 1 : -1;
+                int sign_vy = vy >= 0 ? 1 : -1;
+                int vx_abs = vx >= 0 ? vx : -vx;
+                int vy_abs = vy >= 0 ? vy : -vy;
+                int is_valid = 2;
+
+                for (int step_vx = 0; step_vx <= vx_abs; step_vx++){
+                  for (int step_vy = 0; step_vy <= vy_abs; step_vy++){
+                    int tp_x = px + sign_vx * step_vx;
+                    int tp_y = py + sign_vy * step_vy;
+                    if(access_velocity_traversed_position(vx_abs, vy_abs, step_vx, step_vy))
+                    {  
+                      if (!is_valid_position_nointrinsic(tp_x, tp_y))
+                      {
+                        is_valid = 0;
+                      }
+                    }
+                  }
+                }
+
+                if(is_valid != 0){
+                  is_valid = 1;
+                }
+
+                validity.insert(is_valid);
+              }
+            }
+          }
+        }
+
+        //Create linear_cst
+        var_t pos_x = args_list[0];
+        var_t pos_y = args_list[1];
+
+        abs_dom_t boxes = abs_dom_t::bottom();
+        abs_dom_t conjunction = abs_dom_t::top();
+        if(validity.find(0)!=validity.end() && validity.find(1)!=validity.end()){
+          lin_cst_t cst1(var_isvalid >= number_t(0));
+          lin_cst_t cst2(var_isvalid <= number_t(1));
+          conjunction += cst1;
+          conjunction += cst2;
+        }
+        else if(validity.find(0)!=validity.end()){
+          lin_cst_t cst1(var_isvalid == number_t(0));
+          conjunction += cst1;
+        }
+        else if(validity.find(1)!=validity.end()){
+          lin_cst_t cst2(var_isvalid == number_t(1));
+          conjunction += cst2;
+        }
+        else{
+          lin_cst_t cst2(var_isvalid == number_t(1));
+          conjunction += cst2;
+        }
+        lin_cst_t cst3(pos_x >= number_t(input_bounds[0].first));
+        lin_cst_t cst4(pos_x <= number_t(input_bounds[0].second));
+        lin_cst_t cst5(pos_y >= number_t(input_bounds[1].first));
+        lin_cst_t cst6(pos_y <= number_t(input_bounds[1].second));
+        conjunction += cst3;
+        conjunction += cst4;
+        conjunction += cst5; 
+        conjunction += cst6;
+        boxes |= conjunction;
+
+        crab::outs() << "Validity invariant : " << boxes << "\n\n";
+        new_m_inv |= m_inv&boxes;
+      }
+      
+      m_inv = new_m_inv;
+      auto tmp1 = m_inv.get_content_domain();
+      tmp1.project(cs.get_args());
+
+      crab::outs() << "Projected invariants at exit : " << tmp1 << "\n**********\n";
+
+      
     }
     else{
       // This is the default intrinsic behaviour, here we will test the behaviour of an intrinsic
